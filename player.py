c68062fa8e41b97d33391640cc82d9b0e4efb540
Pweder <wedertriplets@gmail.com>
2023-07-19 12:34:59 +0200

    pycharm commit test

--- player.py
+++ player.py
@@ -1,63 +1,54 @@
-import json
 import requests
 from pprint import pprint
 import urllib3
+
 urllib3.disable_warnings()
 
 
-class player():
+class Player:
 
-    def __init__(self,Name) -> None:
+    def __init__(self, Name) -> None:
 
-        
-        
         self.mainJsonPath = r'mainPlayer.json'
 
-
         requestsUrl = "https://api.spacetraders.io/v2/register"
-        requestHeader = {'Content-Type': 'application/json','Content-Length': '39'}
-        payload = {'symbol': Name,'faction': 'VOID'}
-        currRequest = requests.post(requestsUrl,headers=requestHeader,verify= False,json=payload)
+        requestHeader = {'Content-Type': 'application/json', 'Content-Length': '39'}
+        payload = {'symbol': Name, 'faction': 'VOID'}
+        currRequest = requests.post(requestsUrl, headers=requestHeader, verify=False, json=payload)
 
         self.rawPlayerData = currRequest.json()
-        f = open (self.mainJsonPath,'r')
-        
+        # f = open(self.mainJsonPath, 'r')
+
         # self.rawPlayerData = json.load(f) 
         # pprint(self.rawPlayerData)
 
-
-
         # with open('mainPlayer.json', 'w') as f:
         #     json.dump(self.rawPlayerData, f)
-
 
         self.token = self.rawPlayerData['data']['token']
         self.agent = self.rawPlayerData['data']['agent']
         self.contract = self.rawPlayerData['data']['contract']
         self.faction = self.rawPlayerData['data']['faction']
         self.ships = self.rawPlayerData['data']['ship']
-        
-        
-    def getMyShipIds(self):
+
+    def getMyShipIds(self, returnShips=False):
         # Gets all of your ship ids 
         url = "https://api.spacetraders.io/v2/my/ships"
         headers = {
             "Accept": "application/json",
-            "Authorization": f"Bearer {self.token}"#
-            }
-        response = requests.get(url, headers=headers,verify= False)
-        
+            "Authorization": f"Bearer {self.token}"  #
+        }
+        response = requests.get(url, headers=headers, verify=False)
+
         self.ships = response.json()['data']
 
         idList = []
         for ship in self.ships:
-           idList.append(ship['symbol']) 
+            idList.append(ship['symbol']) if not returnShips else idList.append(ship)
         return idList if idList else None
-    
+
     def getToken(self):
         return self.token
-
-
 
     def getCredits(self):
         url = "https://api.spacetraders.io/v2/my/agent"
@@ -65,18 +56,16 @@ class player():
             "Accept": "application/json",
             "Authorization": f"Bearer {self.token}"
         }
-        response = requests.get(url, headers=headers,verify=False)
+        response = requests.get(url, headers=headers, verify=False)
 
         return response.json()['data']['credits']
 
-    def purchase_ship(self,wishedType,amount = 1):
-        
-        for shipNum in range(1,amount+1):
-            
+    def purchase_ship(self, wishedType, amount=1):
 
+        for shipNum in range(1, amount + 1):
+            pass
 
-# p = player('player313254')
+# p = Player('player313254')
 # c = p.getCredits()
 # print(p.getToken())
 # print(c)
-
