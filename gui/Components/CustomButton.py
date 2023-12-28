074314f8586aa3c8f0119cea859548e12713d9b6
pweder69 <pweder07@charlottesvilleschools.org>
2023-12-28 01:37:34 -0500

    Finished CustomButton.py

--- gui/Components/CustomButton.py
+++ gui/Components/CustomButton.py
@@ -1,87 +1,71 @@
-"""
-Button Implementaion that changes the background image on hover
-"""
-from enum import Enum, auto
 import ttkbootstrap as ttk
-from ttkbootstrap.constants import *
 from ttkbootstrap import PhotoImage
 from Constants import SQUARE_DARK_DEFAULT, DARKLY
 from pathlib import Path
 from PIL import Image, ImageTk
 
 
-class buttonState(Enum):
-    DEFAULT = auto()
-    HOVER = auto()
-
-
-# noinspection PyUnresolvedReferences
 class CustomButton(ttk.Button):
     """
-    Custom button that enables automatic switching of the background image on hover
+    Class that's goal is to implement 2-3 images as the indicators for its condition and sizes to allow custom images
+    to be used-in GUI's
     """
-
     def __init__(self,
-                 master,
-                 defaultPhotoPath: Path,
-                 hoverPhotoPath: Path,
-                 pressedPhotoPath: Path = None,
-                 background=DARKLY["colors"]["bg"],
-                 **kwargs
+                 master: any,
+                 default_photo_path: Path,
+                 hover_photo_path: Path,
+                 pressed_photo_path: Path = None,
+                 background: str = DARKLY["colors"]["bg"],
+                 **kwargs: any
                  ) -> None:
+        """
 
+        Args:
+            master: container
+            default_photo_path: path of the default background photo
+            hover_photo_path: path of the photo displayed on hover
+            pressed_photo_path: optional photo displayed on click
+            background: optional background
+            **kwargs: keword arguments
+        """
         super().__init__(master, **kwargs)
 
-        self.photoPaths = {
-            "default": defaultPhotoPath,
-            "hover": hoverPhotoPath,
+        self.photo_paths = {
+            "default": default_photo_path,
+            "hover": hover_photo_path,
         }
 
+        self.defaultPhotoImage = PhotoImage("default", file=default_photo_path)
+        self.hoverPhotoImage = PhotoImage("hover", file=hover_photo_path)
 
-        self.defaultPhotoImage = PhotoImage("default", file=defaultPhotoPath)
-        self.hoverPhotoImage = PhotoImage("hover", file=hoverPhotoPath)
+        self.photo_images = [self.defaultPhotoImage,
+                             self.hoverPhotoImage]
 
-        self.photoImages = [self.defaultPhotoImage,
-                            self.hoverPhotoImage]
+        if pressed_photo_path:
+            self.pressed_photo_image = PhotoImage("pressed", file=pressed_photo_path)
+            self.photo_images.append(self.pressed_photo_image)
+            self.photo_paths.update({"pressed": pressed_photo_path})
 
-        if pressedPhotoPath:
-            self.pressedPhotoImage = PhotoImage("pressed", file=pressedPhotoPath)
-            self.photoImages.append(self.pressedPhotoImage)
-            self.photoPaths.update({"pressed": pressedPhotoPath})
-
-
-        s = ttk.Style()  # extract to parent class as a class variable so that it is not redefined every time
+        s = ttk.Style()
         s.configure("custom.TButton", focuscolor="", borderwidth=0, bootstyle="secondary", background=background)
+        s.map("custom.TButton",
+              background=[("active", background), ],
+              image=[("active", self.photo_images[1]), ("!active", self.photo_images[0])])
 
-        self.configure(style="custom.TButton", image=self.defaultPhotoImage)
+        self.configure(style="custom.TButton")
 
-        self.bind("<Configure>", self.resize)
-        self.bind("<Enter>", self.onEnter)
-        self.bind("<Leave>", self.onLeave)
+        self.bind("<Configure>", self.__resize)
 
-    def resize(self, event):
+    def __resize(self, event: any) -> None:
         width = self.winfo_width()
         height = self.winfo_height()
-
-        resisedContainer = []
-        for keys,path in self.photoPaths.items():
+        resized_container = []
+        for keys, path in self.photo_paths.items():
             image = Image.open(path)
             image = image.resize((width, height))
             photo = ImageTk.PhotoImage(image)
-            resisedContainer.append(photo)
-        self.photoImages = resisedContainer
-        print(height, width)
-
-
-    def onEnter(self, event):
-        self["image"] = self.hoverPhotoImage
-
-    def onLeave(self, event):
-        self["image"] = self.defaultPhotoImage
-
-    def onLeftClick(self, event):
-        if self.pressedPhotoImage:
-            self["image"] = self.pressedPhotoImage
+            resized_container.append(photo)
+        self.photo_images = resized_container
 
 
 if __name__ == "__main__":
@@ -93,15 +77,12 @@ if __name__ == "__main__":
     D = SQUARE_DARK_DEFAULT / "Square-Dark-Default.png"
     H = SQUARE_DARK_DEFAULT / "Square-Dark-Hover.png"
 
-    defaultImage = ttk.PhotoImage(name="default", file=D)
-    hoverImage = ttk.PhotoImage(name="hover", file=H)
-
     MyButton = CustomButton(window,
                             D,
                             H,
                             compound="center",
                             text="test", )
 
-    MyButton.grid(row=1, column=1, sticky=EW)
+    MyButton.pack(pady=30)
 
     window.mainloop()
